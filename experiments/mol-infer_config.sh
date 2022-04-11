cd2aa0aece5694870bac8e466ad3e68bc1d05686
pomeLo <mix.plugs@gmail.com>
2022-04-11 21:26:41 +0800

    Update mol-infer_config.sh

--- experiments/mol-infer_config.sh
+++ experiments/mol-infer_config.sh
@@ -2,27 +2,27 @@
 # mol-inferのパス
 # パスの末尾に「/」や「\」を付けないでください。
 # 例：
-# Windows    "G:/mol-infer"
-# Linux      "~/mol-infer"
+# Windows          "G:/mol-infer"
+# Linux / MacOS    "/home/mol-infer"
 #
 # Root of mol-infer
 # Do not add "/" or "\" to the end of the path (especially on windows).
 # Examples:
-# Windows    "G:/mol-infer"
-# Linux      "~/mol-infer"
+# Windows          "G:/mol-infer"
+# Linux / MacOS    "/home/mol-infer"
 # +--------------------------------------------------------------------------+ #
 MOLINFER_ROOT="G:/lab/mol-infer"
 
 # +--------------------------------------------------------------------------+ #
 # CPLEXの実行ファイルへのパス
 # 例：
-# Windows    "G:/CPLEX/CPLEX_Studio1210/cplex/bin/x64_win64/cplex.exe"
-# Linux      "/opt/ibm/cplex_12.10/cplex/bin/x86-64_linux/cplex"
+# Windows          "G:/CPLEX/CPLEX_Studio1210/cplex/bin/x64_win64/cplex.exe"
+# Linux / MacOS    "/opt/ibm/cplex_12.10/cplex/bin/x86-64_linux/cplex"
 #
 # Path to CPLEX executable
 # Examples:
-# Windows    "G:/CPLEX/CPLEX_Studio1210/cplex/bin/x64_win64/cplex.exe"
-# Linux      "/opt/ibm/cplex_12.10/cplex/bin/x86-64_linux/cplex"
+# Windows          "G:/CPLEX/CPLEX_Studio1210/cplex/bin/x64_win64/cplex.exe"
+# Linux / MacOS    "/opt/ibm/cplex_12.10/cplex/bin/x86-64_linux/cplex"
 # +--------------------------------------------------------------------------+ #
 CPLEX_PATH="G:/CPLEX/CPLEX_Studio1210/cplex/bin/x64_win64/cplex.exe"
 
